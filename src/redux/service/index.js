953ed6d5dccb3db39cf0ba8841941a77a9005d95
Akanksha <b.akanksha6@gmail.com>
2022-05-07 22:18:17 +0530

    fixed typo

--- src/redux/service/index.js
+++ src/redux/service/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const searchService = async (keyword) => {
   return await axios.get(
-    `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&q=${keyword}$part=snippet&maxResults=10`
+    `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&q=${keyword}&part=snippet&maxResults=10`
   );
 };
