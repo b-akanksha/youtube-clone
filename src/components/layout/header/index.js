0ffdaacbeb29dba6c712173840ec001df32fe259
Akanksha <b.akanksha6@gmail.com>
2022-05-07 21:31:02 +0530

    added search functionality

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { searchThunk } from "../../../redux/thunks";
 import "./header.css";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const [searchText, setSearchText] = React.useState("");
+  const handleChange = (e) => setSearchText(e.target.value);
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      dispatch(searchThunk(searchText));
+    }
+  };
+
   return (
     <div className="header">
-      <input type="search" placeholder="Search.." className="header__search" />
+      <input
+        type="search"
+        placeholder="Search.."
+        className="header__search"
+        value={searchText}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+      />
     </div>
   );
 };
