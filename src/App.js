d1fb43b2bce8645c56c939347073822fc2ae9739
Akanksha <b.akanksha6@gmail.com>
2022-05-07 19:49:17 +0530

    layout added and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Body from "./components/layout/body";
+import Header from "./components/layout/header";
 
 function App() {
   return (
-    <div>
-      <p>Initial Setup</p>
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 }
