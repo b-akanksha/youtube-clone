357baba20bfd95fae08273dc37d4ca236f440b6b
Akanksha <b.akanksha6@gmail.com>
2022-05-07 21:31:27 +0530

    added snackbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { Alert, Snackbar } from "@mui/material";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Body from "./components/layout/body";
 import Header from "./components/layout/header";
 import Loader from "./components/layout/loader";
+import { clearError } from "./redux/actions";
 
 function App() {
-  const { loading } = useSelector((state) => state.youtubeify);
+  const { loading, error } = useSelector((state) => state.youtubeify);
+  const dispatch = useDispatch();
+
+  const handleClose = () => dispatch(clearError());
+
   return (
-    <div className="app">
-      <Header />
-      {loading ? <Loader /> : <Body />}
-    </div>
+    <>
+      <div className="app">
+        <Header />
+        {loading ? <Loader /> : <Body />}
+      </div>
+      <Snackbar open={error.open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert severity="error" onClose={handleClose} variant="filled">
+          {error.code}: {error.message}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
