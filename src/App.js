5c4ce2fcb81a7e8c4eb76f2253b46136defa1c55
Akanksha <b.akanksha6@gmail.com>
2022-05-07 20:23:25 +0530

    loader  added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Body from "./components/layout/body";
 import Header from "./components/layout/header";
+import Loader from "./components/layout/loader";
 
 function App() {
+  const { loading } = useSelector((state) => state.youtubeify);
   return (
     <div className="app">
       <Header />
-      <Body />
+      {loading ? <Loader /> : <Body />}
     </div>
   );
 }
